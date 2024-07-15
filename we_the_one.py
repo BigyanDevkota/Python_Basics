1308c335ab1721a74ee6aa3a9b9c698528b1a99a
BigyanDevkota <bigyandevkota11@gmail.com>
2024-07-15 21:57:27 +0545

    first commit

--- we_the_one.py
+++ we_the_one.py
@@ -1,2 +1,14 @@
 # ygsjaauksak
 
+# A default function for Prime checking conditions  
+def PrimeChecker(a):  
+    # Checking that given number is more than 1  
+    if a > 1:  
+        # Iterating over the given number with for loop  
+        for j in range(2, int(a/2) + 1):  
+            # If the given number is divisible or not  
+            if (a % j) == 0:  
+                print(a, "is not a prime number")  
+                break  
+        # Else it is a prime number  
+ 
